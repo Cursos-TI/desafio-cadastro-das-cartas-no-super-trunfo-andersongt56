356c72f4cdfcfe8cbb58692b70392e8564c1502a
andersongt56 <anderson_silvasouza@outlook.com>
2025-02-25 23:44:05 +0000

    criei CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,76 @@
 #include <stdio.h>
+#include <string.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main (){
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+char estado[3];
+char codigo[50];
+char cidade[50];
+float pib;
+float area;
+int numero;
+int populacao;
 
-    return 0;
+char estado2[3];
+char codigo2[50];
+char cidade2[50];
+float pib2;
+float area2;
+int numero2;
+int populacao2;
+
+
+
+printf("digite seu estado:\n");
+scanf("%s[^\n]", &estado);
+
+printf("letra do estado seguida de 1 a 4:\n");
+scanf("%s", &codigo);
+
+printf("nome da cidade:\n");
+scanf(" %s[^\n]", &cidade);
+
+printf("pib:\n");
+scanf("%f", &pib);
+
+printf("area:\n");
+scanf("%f", &area);
+
+printf("numero de ponto turístico:\n");
+scanf("%d", &numero);
+
+printf("população:\n");
+scanf("%d", &populacao);
+
+
+//============================================================== carta 2
+
+
+
+printf("digite seu segundo estado:\n");
+scanf("%s[^\n]", &estado2);
+
+printf("codigo da carta seguida de um número de 1 á 4:\n");
+scanf("%s", &codigo2);
+
+printf("nome da segunda cidade:\n");
+scanf("%s[^\n]", &cidade2);
+
+printf("pib:\n");
+scanf("%f", &pib2);
+
+printf("área:\n");
+scanf("%f", &area2);
+
+printf("número de ponto turístico:\n");
+scanf("%d", &numero2);
+
+printf("população:\n");
+scanf("%d", &populacao2);
+
+return 0;
+
 }
+
+
+
